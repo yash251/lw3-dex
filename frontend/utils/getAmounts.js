f30f0dd7a6c77a381cc5067caa131d197b8586e9
Yash Jagtap <jagtapyash2512@gmail.com>
2022-10-31 22:27:23 +0530

    getEtherBalance fxn

--- frontend/utils/getAmounts.js
+++ frontend/utils/getAmounts.js
@@ -5,3 +5,23 @@ import {
   TOKEN_CONTRACT_ABI,
   TOKEN_CONTRACT_ADDRESS,
 } from "../constants";
+
+export const getEtherBalance = async (provider, address, contract = false) => {
+    try {
+        // If the caller has set the `contract` boolean to true, retrieve the balance of
+        // ether in the `exchange contract`, if it is set to false, retrieve the balance
+        // of the user's address
+        if (contract) {
+            const balance = await provider.getBalance(EXCHANGE_CONTRACT_ADDRESS);
+            return balance;
+        }
+        else {
+            const balance = await provider.getBalance(address);
+            return balance;
+        }
+    }
+    catch (err) {
+        console.error(err);
+        return 0;
+    }
+};
