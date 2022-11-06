e0bd32fc0d6378748d376a5ed8cba3995cff1359
Yash Jagtap <jagtapyash2512@gmail.com>
2022-11-06 21:40:09 +0530

    removeLiquidity

--- frontend/utils/removeLiquidity.js
+++ frontend/utils/removeLiquidity.js
@@ -3,3 +3,18 @@ import {
   EXCHANGE_CONTRACT_ABI,
   EXCHANGE_CONTRACT_ADDRESS,
 } from "../constants";
+
+/**
+ * removeLiquidity: Removes the `removeLPTokensWei` amount of LP tokens from
+ * liquidity and also the calculated amount of `ether` and `CD` tokens
+ */
+export const removeLiquidity = async (signer, removeLPTokensWei) => {
+    // Create a new instance of the exchange contract
+    const exchangeContract = new Contract(
+        EXCHANGE_CONTRACT_ADDRESS,
+        EXCHANGE_CONTRACT_ABI,
+        signer
+    );
+    const tx = await exchangeContract.removeLiquidity(removeLPTokensWei);
+    await tx.wait();
+};
