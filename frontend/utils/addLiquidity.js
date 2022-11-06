0bd476da91da997074f353cf81bace245a319b38
Yash Jagtap <jagtapyash2512@gmail.com>
2022-11-06 21:38:33 +0530

    calculateCD

--- frontend/utils/addLiquidity.js
+++ frontend/utils/addLiquidity.js
@@ -46,4 +46,29 @@ import {
     catch (err) {
         console.error(err);
     }
+ };
+
+ /**
+ * calculateCD calculates the CD tokens that need to be added to the liquidity
+ * given `_addEtherAmountWei` amount of ether
+ */
+export const calculateCD = async (
+    _addEther = "0",
+    etherBalanceContract,
+    cdTokenReserve
+  ) => {
+    // `_addEther` is a string, we need to convert it to a Bignumber before we can do our calculations
+    // We do that using the `parseEther` function from `ethers.js`
+    const _addEtherAmountWei = utils.parseEther(_addEther);
+  
+    // Ratio needs to be maintained when we add liquidty.
+    // We need to let the user know for a specific amount of ether how many `CD` tokens
+    // He can add so that the price impact is not large
+    // The ratio we follow is (amount of Crypto Dev tokens to be added) / (Crypto Dev tokens balance) = (Eth that would be added) / (Eth reserve in the contract)
+    // So by maths we get (amount of Crypto Dev tokens to be added) = (Eth that would be added * Crypto Dev tokens balance) / (Eth reserve in the contract)
+  
+    const cryptoDevTokenAmount = _addEtherAmountWei
+        .mul(cdTokenReserve)
+        .div(etherBalanceContract);
+    return cryptoDevTokenAmount;
 };
