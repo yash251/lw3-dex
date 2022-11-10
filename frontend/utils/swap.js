b1376a5337ae047013d9006183afee3830008673
Yash Jagtap <jagtapyash2512@gmail.com>
2022-11-10 11:52:19 +0530

    swapTokens

--- frontend/utils/swap.js
+++ frontend/utils/swap.js
@@ -47,3 +47,54 @@ export const getAmountOfTokensReceivedFromSwap = async (
   
     return amountOfTokens;
 };
+
+/*
+  swapTokens: Swaps `swapAmountWei` of Eth/Crypto Dev tokens with `tokenToBeReceivedAfterSwap` amount of Eth/Crypto Dev tokens.
+*/
+export const swapTokens = async (
+    signer,
+    swapAmountWei,
+    tokenToBeReceivedAfterSwap,
+    ethSelected
+  ) => {
+    // Create a new instance of the exchange contract
+    const exchangeContract = new Contract(
+        EXCHANGE_CONTRACT_ADDRESS,
+        EXCHANGE_CONTRACT_ABI,
+        signer
+    );
+    const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        signer
+    );
+    let tx;
+    // If Eth is selected call the `ethToCryptoDevToken` function else
+    // call the `cryptoDevTokenToEth` function from the contract
+    // As you can see you need to pass the `swapAmount` as a value to the function because
+    // it is the ether we are paying to the contract, instead of a value we are passing to the function
+    if (ethSelected) {
+        tx = await exchangeContract.ethToCryptoDevToken(
+            tokenToBeReceivedAfterSwap,
+            {
+                value: swapAmountWei,
+            }
+        );
+    }
+    else {
+        // User has to approve `swapAmountWei` for the contract because `Crypto Dev` token
+        // is an ERC20
+        tx = await tokenContract.approve(
+            EXCHANGE_CONTRACT_ADDRESS,
+            swapAmountWei.toString()
+        );
+        await tx.wait();
+        // call cryptoDevTokenToEth function which would take in `swapAmountWei` of `Crypto Dev` tokens and would
+        // send back `tokenToBeReceivedAfterSwap` amount of `Eth` to the user
+        tx = await exchangeContract.cryptoDevTokenToEth(
+            swapAmountWei,
+            tokenToBeReceivedAfterSwap
+        );
+    }
+    await tx.wait();
+};
